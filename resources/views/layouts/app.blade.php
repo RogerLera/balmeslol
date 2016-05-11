fb04c574b7a2175557fc17323c23c3a113eed27a
Holabondia <orphen_266@hotmail.com>
2016-05-11 20:58:16 +0200

    modificacion del main view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo asset('bootstrap/css/bootstrap.css')?>" type="text/css"> 
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
@@ -23,22 +23,178 @@
         .fa-btn {
             margin-right: 6px;
         }
+        .input-group-btn select {
+    border-color: #ccc;
+margin-top: 0px;
+    margin-bottom: 0px;
+    padding-top: 7px;
+    padding-bottom: 7px;
+}
+
     </style>
 </head>
 <body id="app-layout">
-    <nav class="navbar navbar-default">
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Task List
-                </a>
-            </div>
-
+<div class="container">
+      <div class="row" style="margin-top:10px;">
+        <div class="col-md-6">
+         <img src="<?php echo asset('images/banner.png')?>">
         </div>
-    </nav>
-
+        <div class="col-md-6">
+          <form class="navbar-form navbar-right" role="search">
+            <div class="form-group">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Buscar jugador">
+                    <span class="input-group-btn">
+                        <select class="btn" data-width="fit">
+                            <option value="euw">EU Oeste</option>
+                            <option value="eune">EU Nórdica y Este</option>
+                            <option value="na">Norteamérica</option>
+                            <option value="lan">Latinoamérica Norte</option>
+                            <option value="las">Latinoamérica Sur</option>
+                            <option value="br">Brasil</option>
+                            <option value="jap">Japón</option>
+                            <option value="ru">Rusia</option>
+                            <option value="tr">Turquía</option>
+                            <option value="oce">Oceanía</option>
+                            <option value="kr">Republica de Corea</option>
+                        </select>
+                    </span>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-info">
+                <i class="fa fa-search" aria-hidden="true"></i>
+            </button>
+          </form>
+        </div>
+      </div>
+ 
+      <div class="row">
+        <nav class="navbar navbar-default" role="navigation">
+            <ul class="nav navbar-nav">
+                <!-- Guias menú -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Guías de campeones<b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-header">
+                            Guías de campeones
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#">Todas las guías</a>
+                        </li>
+                        <li>
+                            <a href="#">Nuevo</a>
+                        </li>
+                        <li>
+                            <a href="#">Popular</a>
+                        </li>
+                        <li>
+                            <a href="#">Más votado</a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- Campeones menú -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Campeones<b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-header">
+                            Campeones
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#">Todos los campeones</a>
+                        </li>
+                        <li>
+                            <a href="#">Últimos campeones</a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- Objetos menú -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Objetos<b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-header">
+                            Objetos
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#">Todos los objetos</a>
+                        </li>
+                        <li>
+                            <a href="#">Últimos objetos</a>
+                        </li>
+                    </ul>
+                </li>
+                <!-- Estadísticas menú -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        Estadísticas<b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-header">
+                            Estadísticas
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#">Popularidad de campeones</a>
+                        </li>
+                        <li>
+                            <a href="#">Popularidad de hechizos</a>
+                        </li>
+                        <li>
+                            <a href="#">Bloqueo de campeones</a>
+                        </li>
+                        <li>
+                            <a href="#">Ratio de victorias</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+      </div>
+</div>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
+<p>loreooerwowsdoosdoasd</p>
     @yield('content')
 
     <!-- JavaScripts -->
