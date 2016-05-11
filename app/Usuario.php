e6a120f938be4588ba0c8f312a89fdb3aec0a366
Roger Lera <RogerLera93@gmail.com>
2016-05-11 20:30:13 +0200

    Modificadas/Eliminadas clases modelo.

--- app/Usuario.php
+++ app/Usuario.php
@@ -4,7 +4,27 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+* Classe Usuario que representa a una persona, la qual puede hacer distintas cosas
+* (iniciar sessión, enviar mensajes, crear guias, editar perfil, ...) en la web.
+*/
 class Usuario extends Model
 {
-    //
+    /**
+     * Atributos de la classe que son assignables.
+     *
+     * @var $fillable: array con los valores que se pueden modificar.
+     */
+    protected $fillable = [
+        'usuAlias', 'usuEmail', 'usuFdn', 'usuPswd',
+    ];
+
+    /**
+     * Atributos ocultos.
+     *
+     * @var $hidden: array con los valores ocultos.
+     */
+    protected $hidden = [
+        'usuPswd', 'remember_token',
+    ];
 }
