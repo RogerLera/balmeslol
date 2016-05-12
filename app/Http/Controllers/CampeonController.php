f57d7012f7dbfdcbb8de76f3abf5f21a9b02d4fb
Roger Lera <RogerLera93@gmail.com>
2016-05-12 18:49:25 +0200

    Editado metodo para cojer array con los campeones.

--- app/Http/Controllers/CampeonController.php
+++ app/Http/Controllers/CampeonController.php
@@ -25,17 +25,23 @@ class CampeonController extends Controller
 		]);
 	}
 
-	/*
-	* Método
+	/**
+	* Método que obtiene todos los campeones, en el idioma que se está visualizando la pàgina.
 	*
+	*
+	* @return array associativo con la información de los campeones.
 	*/
 	public function obtenerCampeones()
 	{
 		// Más adelante implementat sessión con idioma.
 		//$idioma = Session::get('idioma');
+		// Obtenemos el json.
 		$json = file_get_contents('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?locale=es_ES&champData=image&api_key=a9a09074-95bd-4038-addb-a8b5e616e9c6');
+		// Lo transformamos a objetos que php pueda entender.
 		$data = json_decode($json);
+		// Creamos el array que almazenará los campeones.
 		$campeones = array();
+		// En la variable campeones vamos introduciendo cada campeón.
 		foreach($data->data as $infoCampeon){
 		    $campeones[] = array(
 		            'id' => $infoCampeon->id,
@@ -44,6 +50,7 @@ class CampeonController extends Controller
 		            'imagen' => 'http://ddragon.leagueoflegends.com/cdn/6.9.1/img/champion/' . $infoCampeon->image->full,
 		        );
 		}
+		// Devolvemos el array.
 		return $campeones;
 	}
 }
