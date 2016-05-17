bc9296a1a7dd9840aa6e63dfb50692196d16cccf
Holabondia <orphen_266@hotmail.com>
2016-05-17 20:42:53 +0200

    modificacion de infocampeon y agregadas funciones de busqueda

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,10 +19,7 @@
 <body id="app-layout">
     <div class="container">
         <div class="row banner">
-            <div class="col-md-6">
-
-            </div>
-            <div class="col-md-6" style="padding-top:30px;">
+            <div class="col-md-6 col-md-offset-6" style="padding-top:30px;">
                 <form class="navbar-form navbar-right" role="search">
                     <div class="form-group">
                         <input type="text" class="form-control" placeholder="Buscar jugador">
@@ -45,144 +42,130 @@
                     </div>
                 </form>
             </div>
-      </div>
+        </div>
 
-      <div class="row">
-
-        <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-                              <!-- Guias menú -->
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Guías de campeones<b class="caret"></b>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-header">
-                            Guías de campeones
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">Todas las guías</a>
-                        </li>
-                        <li>
-                            <a href="#">Nuevo</a>
-                        </li>
-                        <li>
-                            <a href="#">Popular</a>
-                        </li>
-                        <li>
-                            <a href="#">Más votado</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- Campeones menú -->
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Campeones<b class="caret"></b>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-header">
-                            Campeones
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="{{ url('/campeones') }}">Todos los campeones</a>
-                        </li>
-                        <li>
-                            <a href="#">Últimos campeones</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- Objetos menú -->
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Objetos<b class="caret"></b>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-header">
-                            Objetos
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">Todos los objetos</a>
-                        </li>
-                        <li>
-                            <a href="#">Últimos objetos</a>
-                        </li>
-                    </ul>
-                </li>
-                <!-- Estadísticas menú -->
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        Estadísticas<b class="caret"></b>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-header">
-                            Estadísticas
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="#">Popularidad de campeones</a>
-                        </li>
-                        <li>
-                            <a href="#">Popularidad de hechizos</a>
-                        </li>
-                        <li>
-                            <a href="#">Bloqueo de campeones</a>
-                        </li>
-                        <li>
-                            <a href="#">Ratio de victorias</a>
-                        </li>
-                    </ul>
-                </li>
-                </ul>
-              </li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-               <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li>
-                        <a href="{{ url('/usuario/inicioSesion') }}">Iniciar</a>
-                    </li>
-                    <li>
-                        <a href="{{ url('/usuario/registro') }}">Registrarse</a>
-                    </li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::usuario()->usuAlias }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/usuario/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Salir</a></li>
+        <div class="row">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <!-- Guias menú -->
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    Guías de campeones<b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">
+                                        Guías de campeones
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li>
+                                        <a href="#">Todas las guías</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Nuevo</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Popular</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Más votado</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <!-- Campeones menú -->
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    Campeones<b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">
+                                        Campeones
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li>
+                                        <a href="{{ url('/campeones') }}">Todos los campeones</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Últimos campeones</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <!-- Objetos menú -->
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    Objetos<b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">
+                                        Objetos
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li>
+                                        <a href="#">Todos los objetos</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Últimos objetos</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <!-- Estadísticas menú -->
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    Estadísticas<b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="dropdown-header">
+                                        Estadísticas
+                                    </li>
+                                    <li class="divider"></li>
+                                    <li>
+                                        <a href="#">Popularidad de campeones</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Popularidad de hechizos</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Bloqueo de campeones</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Ratio de victorias</a>
+                                    </li>
+                                </ul>
+                            </li>
                         </ul>
-                    </li>
-                @endif
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </nav>
+                        <ul class="nav navbar-nav navbar-right">
+                           <!-- Authentication Links -->
+                            @if (Auth::guest())
+                                <li>
+                                    <a href="{{ url('/usuario/inicioSesion') }}">Iniciar</a>
+                                </li>
+                                <li>
+                                    <a href="{{ url('/usuario/registro') }}">Registrarse</a>
+                                </li>
+                            @else
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                        {{ Auth::usuario()->usuAlias }} <span class="caret"></span>
+                                    </a>
 
-  </div>
-</div>
-
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="{{ url('/usuario/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Salir</a></li>
+                                    </ul>
+                                </li>
+                            @endif
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div><!--/.container-fluid -->
+            </nav>
+        </div><!--/.row -->
+    </div><!--/.container -->
     @yield('content')
-
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
+    <script src="<?php echo asset('js/myjsfunctions.js')?>"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
