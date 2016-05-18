39f0e26d13ef7b190769d84efa90c0df4180b628
Roger Lera <RogerLera93@gmail.com>
2016-05-18 16:19:19 +0200

    Mejorado la funcionalidad de buscar por rol.

--- public/js/myjsfunctions.js
+++ public/js/myjsfunctions.js
@@ -24,9 +24,17 @@ $(document).ready(function() {
         // Hacemos un bucle por cada campeón.
         $(".champ-box").each(function() {
             // Recojemos en una variable los valores de los inputs hidden.
-            var rolCampeon = $(this).find('[name="tag"]').val();
-            // Comparamos con el filtro si coincide con el rol del campeón.
-            if (filtro === rolCampeon) {
+            var rolCampeon = $(this).find('[name="tag"]');
+            // Creamos variable rolCampeon2 (ya que algunos campeones tienen 2 roles).
+            var rolCampeon2 = null;
+            // Comprobamos que el campeón tiene segundo rol, si el segundo objeto que nos llega (el hidden),
+            // es undefined o no.
+            if (typeof rolCampeon[1] !== 'undefined') {
+                // Si no es undefined, introducimos el valor en la variable.
+                rolCampeon2 = rolCampeon[1].defaultValue;
+            }
+            // Comprovamos si en alguno de los dos input existe el rol marcado.
+            if (filtro === rolCampeon[0].defaultValue || filtro === rolCampeon2) {
                 // Si coincide lo mostramos.
                 $(this).show();
             } else {
