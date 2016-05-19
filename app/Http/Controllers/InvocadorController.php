101c8e2c70be0170ed3b7fa3dd2ce38653311dae
Ruben <ruben.20.a@gmail.com>
2016-05-19 16:16:09 +0200

    hechizo controller y invocador en camino

---
+++ app/Http/Controllers/InvocadorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+/**
+ * Clase CampeonController que llama la vista para mostrar los datos referente a los
+ * campeones, obteniendo la información de la API Riot en formato .json.
+ */
+class InvocadorController extends Controller {
+    
+}
+
