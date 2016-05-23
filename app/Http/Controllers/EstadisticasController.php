8c97637cd41138c83b209d40549685cbc7722991
Ruben <ruben.20.a@gmail.com>
2016-05-23 15:40:46 +0200

    estadisticas controller incompleto

--- app/Http/Controllers/EstadisticasController.php
+++ app/Http/Controllers/EstadisticasController.php
@@ -12,5 +12,27 @@ use App\Http\Requests;
  */
 class EstadisticasController extends Controller {
     
+    
+    /**
+     * Método principal que carga las estadisticas de las partidas featured del momento a la base de datos
+     * Guardaremos datos de: uso de campeones, uso de hechizos y bans de campeones
+     */
+    public function guardaEstadisticas(){
+        
+        $json = file_get_contents('https://euw.api.pvp.net/observer-mode/rest/featured?api_key=1a7388f5-a5a6-4adf-9f7b-cc4e0ae49c6e');
+
+        $listaFeatured = json_decode($json);
+
+        foreach ($listaFeatured->gameList as $partidas) {
+            foreach ($partidas->participants as $jug) {
+                $spell1 = $jug->spell1Id;
+                $spell2 = $jug->spell2Id;
+                $escUsado = $jug->championId;
+            }
+            foreach ($partidas->bannedChampions as $bans) {
+                $escBloqueado = $bans->championId;
+            }
+        }
+    }
 }
 
