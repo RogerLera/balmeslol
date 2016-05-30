2a17d98e00cc5c2aac93f9a3bf4ab79fbded7723
RogerLera <RogerLera93@gmail.com>
2016-05-30 11:33:37 +0200

    Modificado que pueda cojer el div entero en runas (imagen y texto).

--- public/js/imagenesGuias.js
+++ public/js/imagenesGuias.js
@@ -1,18 +1,26 @@
 var contenido;
 if ($('div').hasClass('borde')) {
-    contenido = 'ok';
-    console.log(contenido);
+    $('div.borde').on('click', function(e) {
+
+        contenido = 'ok';
+        console.log(e);
+        // Accedemos a la ventana padre del iframe (popup), i en el tinyMCE, executamos el
+        // comando de inserir contenido, introduciendo la imagen.
+        window.parent.tinyMCE.activeEditor.execCommand('mceInsertContent', 0, contenido);
+        // Finalmente cerramos el iframe.
+        window.parent.tinyMCE.activeEditor.windowManager.close();
+    });
 } else {
     // Quando se haga click en una imagen del popup, pasaremos el elemento selecionado por la función.
-    $('img').on("click", function(e) {
+    $('img').on('click', function(e) {
         // Guardamos en la variable imagen un string de un tag '<img>' con el src y el
         // alt de la imagen que hemos apretado.
         contenido = '<img style="width:50px;height:50px;" src="' + e.currentTarget.src +
             '" alt="' + e.currentTarget.alt + '">';
+        // Accedemos a la ventana padre del iframe (popup), i en el tinyMCE, executamos el
+        // comando de inserir contenido, introduciendo la imagen.
+        window.parent.tinyMCE.activeEditor.execCommand('mceInsertContent', 0, contenido);
+        // Finalmente cerramos el iframe.
+        window.parent.tinyMCE.activeEditor.windowManager.close();
     });
 }
-// Accedemos a la ventana padre del iframe (popup), i en el tinyMCE, executamos el
-// comando de inserir contenido, introduciendo la imagen.
-window.parent.tinyMCE.activeEditor.execCommand('mceInsertContent', 0, contenido);
-// Finalmente cerramos el iframe.
-window.parent.tinyMCE.activeEditor.windowManager.close();
