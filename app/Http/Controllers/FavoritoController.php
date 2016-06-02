326c3ba52578c0449540ceac760b83d47943688a
RogerLera <RogerLera93@gmail.com>
2016-06-02 11:41:17 +0200

    Creados los métodos para añadir/eliminar favoritos (antes estaban en GuiaController).

--- app/Http/Controllers/FavoritoController.php
+++ app/Http/Controllers/FavoritoController.php
@@ -3,10 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Input;
 use App\Http\Requests;
+use App\Favorito;
 
-class FavoritoController extends Controller
-{
-    //
+/**
+* Clase FavoritoController que se encarga de añadir/eliminar un registro a la base
+* de datos, dependiendo si se ha dado al boton para guardar a favoritos una guia o
+* para eliminarla.
+*/
+class FavoritoController extends Controller {
+
+    public function guardarAFavoritos()
+    {
+        $guiId = Input::get('guiId');
+        $usuId = Input::get('usuId');
+        $resultado = 'No se ha podido guardar a favoritos.';
+        $where = ['usuId' => $usuId, 'guiId' => $guiId];
+        $favoritoExistente = Favorito::where($where)->first();
+        if (is_null($favoritoExistente)) {
+            Favorito::create([
+                'usuId' => $usuId,
+                'guiId' => $guiId,
+            ]);
+            $resultado = 'Guardado a favoritos.';
+        }
+        return $resultado;
+    }
+
+    public function borrarDeFavoritos()
+    {
+        $guiId = Input::get('guiId');
+        $usuId = Input::get('usuId');
+        $resultado = 'No se ha podido borrar de favoritos.';
+        $where = ['usuId' => $usuId, 'guiId' => $guiId];
+        $favoritoExistente = Favorito::where($where)->delete();
+        $resultado = 'Borrado de favoritos.';
+        return $resultado;
+    }
 }
