4d718a7d7f756a5748c57ea5c2123a283b2d53db
RogerLera <RogerLera93@gmail.com>
2016-06-03 12:17:12 +0200

    Añadido que use TraitObjetos para metodo obtenerObjetos (el mismo método estaba 2 veces).

--- app/Http/Controllers/ObjetoController.php
+++ app/Http/Controllers/ObjetoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Traits\TraitVersionActual;
+use App\Traits\TraitObjetos;
 use Config;
 
 /**
@@ -13,7 +14,7 @@ use Config;
  */
 class ObjetoController extends Controller
 {
-    use TraitVersionActual;
+    use TraitVersionActual, TraitObjetos;
 
     /**
      * Método principal que se llama al acceder a la pestanya objetos.
@@ -36,37 +37,6 @@ class ObjetoController extends Controller
         return view('objetos.infoObjeto', [
             'objeto' => $this->obtenerObjetoPorId($id),
         ]);
-    }
-
-    /**
-     * Método que obtiene todos los objetos, en el idioma que se está visualizando la pàgina.
-     *
-     *
-     * @return array associativo con la información de los objetos.
-     */
-    public function obtenerObjetos() {
-        $idioma = Config::get("app.locale");
-        // Obtenemos el json y lo parseamos a objeto php.
-        $json = file_get_contents('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/item?locale='.$idioma.'&itemListData=image,tags&api_key=1a7388f5-a5a6-4adf-9f7b-cc4e0ae49c6e');
-        $data = json_decode($json);
-
-        // Creamos el array contenedor de todos los objetos.
-        $objetos = array();
-        // En la variable objetos vamos introduciendo cada objeto.
-        foreach ($data->data as $infoObjeto) {
-            $objetos[] = array(
-                // Comprovamos que existe el atributo name (hay un objeto que no tiene nombre).
-                'nombre' => (isset($infoObjeto->name)) ? $infoObjeto->name : "Farsight Orb",
-                'id' => $infoObjeto->id,
-                'imagen' => 'https://ddragon.leagueoflegends.com/cdn/' . $data->version . '/img/item/' . $infoObjeto->image->full,
-                // Comprovamos que tienen etiquetas (hay muchos objetos que no tienen etiquetas).
-                'tags' => (isset($infoObjeto->tags)) ? $infoObjeto->tags : "other",
-            );
-        }
-        // Ordenamos el array por nombre.
-        asort($objetos);
-        // Devolvemos el objeto.
-        return $objetos;
     }
 
     /**
